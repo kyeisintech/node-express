9e901228fc9b27538c9b0451fc43c4629eab6c6e
Kyeisha W S <kyeisintech@gmail.com>
2022-01-22 12:51:35 -0500

    Week 1 Workshop

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -9,7 +9,7 @@ campsiteRouter
     next();
   })
   .get((req, res) => {
-    res.end("Will send all the campsites to you");
+    res.end("Will send all campsites to you");
   })
   .post((req, res) => {
     res.end(
@@ -24,4 +24,30 @@ campsiteRouter
     res.end("Deleting all campsites");
   });
 
+campsiteRouter
+  .route("/:campsiteId")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((req, res) => {
+    res.end(
+      `Will send all of the details of the campsite ${req.params.campsiteId} to you`
+    );
+  })
+  .post((req, res) => {
+    res.statusCode = 403;
+    res.end(
+      `POST operation not supported on /campsites/${req.params.campsiteId}`
+    );
+  })
+  .put((req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
+  })
+  .delete((req, res) => {
+    res.end(`Deleting campsite ${req.params.campsiteId}`);
+  });
 module.exports = campsiteRouter;
